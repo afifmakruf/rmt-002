81aabf80ac874bf1c978395f4d2fa0f9354992d4
afifmakruf <afifmakruf99@gmail.com>
2024-11-12 22:29:29 +0700

    Tambahan

---
+++ python/person.py
@@ -0,0 +1,10 @@
+name = 'Afif'
+devices = ['HP', 'PC', 'Laptop']
+
+def display(name):
+    print(f'Halo {name}')
+
+def hitung_segitiga(bilangan_1, bilangan_2):
+  return bilangan_1 * bilangan_2 / 2
+def hitung_persegi(bilangan_1, bilangan_2):
+  return bilangan_1 * bilangan_2
